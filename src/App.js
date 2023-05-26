bb3fb40145895a4fd1517f2204efb333a9fafef4
malshan_r <malshan.edu@gmail.com>
2023-05-26 14:52:17 +0530

    tested how react works

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MyButton from './Components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,18 @@
+export default function MyButton() {
+
+    return (
+      <div class="container">
+          <div class="row">
+              <div class="col-12 text-center">
+                <button class="btn btn-warning">
+                  Click Me
+                </button>
+              </div>
+          </div>
+      </div>
+
+    );
+}
+
+
+ 
